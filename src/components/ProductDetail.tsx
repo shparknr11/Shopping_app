7570e0d9cb41977c2c9aac161708be2d4ae1ae14
shparknr11 <shparknr11@naver.com>
2024-05-27 13:10:57 +0900

    docs: typescript 마이그레이션 진행중

--- src/components/ProductDetail.tsx
+++ src/components/ProductDetail.tsx
@@ -1,51 +1,89 @@
 import { useState, useEffect } from "react";
-import { getProductList } from "../api/productListApi";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { getProductOne } from "../api/productItemApi";
+import useCart from "../hooks/useCart";
+import useModal from "../hooks/useModal";
+import Modal from "./common/Modal";
+import Button from "./common/Button";
 import { ProductType } from "../types/product";
 
-const ProductList = () => {
+const ProductDetail = () => {
   const navigate = useNavigate();
+  const { productId } = useParams<{ productId: string }>();
+  const { addCarts } = useCart();
+  const { isModalOpen, modalMessage, confirmAction, openModal, closeModal } =
+    useModal();
 
-  const [productList, setProductList] = useState<ProductType[]>([]);
+  const [product, setProduct] = useState<ProductType | null>(null);
   const [isLoading, setIsLoading] = useState(false);
+
+  const handleMoveModifyPage = () => {
+    if (productId) {
+      navigate(`/modify/${productId}`);
+    }
+  };
+  const handleMovePurchasePage = () => {
+    if (productId) {
+      navigate(`/purchase/${productId}`);
+    }
+  };
+
+  const handleCartAdd = () => {
+    if (product) {
+      addCarts(product.id);
+      openModal({
+        message: "장바구니에 성공적으로 추가하였습니다!",
+        onConfirm: () => {
+          closeModal();
+          navigate("/cart");
+        },
+      });
+    }
+  };
 
   useEffect(() => {
     setIsLoading(true);
 
-    getProductList()
-      .then(response => {
-        // console.log(response?.data.products);
-        const data = response?.data.products;
-        setProductList(data);
-      })
-      .catch(error => console.log(error))
-      .finally(() => setIsLoading(false));
-  }, []);
+    if (productId) {
+      getProductOne(productId)
+        .then(response => {
+          const data = response?.data.product;
+          setProduct(data);
+          // console.log(data);
+        })
+        .catch(error => console.log(error))
+        .finally(() => setIsLoading(false));
+    }
+  }, [productId]);
 
   if (isLoading) {
-    return <h3>상품 목록을 불러오는중입니다...</h3>;
+    return <h3>해당 상품 정보를 불러오는중입니다...</h3>;
+  }
+
+  if (!product) {
+    return <h3>찾으시는 상품이 없습니다.</h3>;
   }
 
   return (
     <div>
-      <h2>상품 목록 보여주기</h2>
+      <h2>상품 상세보기</h2>
+      <div>{product.name}</div>
+      <div>{product.price.toLocaleString("KO-kr")}원</div>
+      <div>{product.explanation}</div>
 
-      <div>
-        {productList.map(item => (
-          <ul key={item.id}>
-            <li
-              style={{ cursor: "pointer" }}
-              onClick={() => navigate(`/product/${item.id}`)}
-            >
-              {item.name}
-            </li>
-            <li>{item.price.toLocaleString("KO-kr")}원</li>
-            <li>{item.explanation}</li>
-          </ul>
-        ))}
-      </div>
+      <Button label="상품 수정하기" onClick={handleMoveModifyPage} />
+      <Button label="상품 구매하기" onClick={handleMovePurchasePage} />
+      <Button label="장바구니에 담기" onClick={handleCartAdd} />
+
+      {/* 모달 관련 */}
+      <Modal
+        isOpen={isModalOpen}
+        message={modalMessage}
+        onClose={closeModal}
+        onConfirm={confirmAction}
+      />
     </div>
   );
 };
 
-export default ProductList;
+export default ProductDetail;
